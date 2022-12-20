52142a672ec80e7402d85233c80c1af8da4dfb41
Davi S <67160664+Davi-S@users.noreply.github.com>
2022-12-20 19:27:55 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,8 @@
 CLIP_SAVING_PATH = ""
 # CLIP_SAVING_PATH example: "C:\\Users\\YourName\\Videos\\Clips\\"
 
-CAPTURE_INTERVAL = 0.2
-CAPTURE_PAST_TIME = 10
+CAPTURE_INTERVAL = 0.2  # seconds
+CAPTURE_PAST_TIME = 10  # seconds
 
 HOT_KEYS = {'trigger': 'insert', 'quit': 'end'}
+# HOT_KEYS: only change the values. Do not change the keys
